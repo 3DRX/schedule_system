3f2cc27138524a7e548f7d27b1a0f5a803433f66
3DRX <kjy20030228@icloud.com>
2023-02-23 18:10:29 +0800

    完成登陆前端界面

--- app/src/main/frontend/src/components/LoginPage.jsx
+++ app/src/main/frontend/src/components/LoginPage.jsx
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
-import styles from './LoginPage.css';
-import profilePhoto from './image/exampleProfilePhoto.jpg';
-import background from './image/exampleMaterial_1.jpg';
+import './LoginPage.css';
 
 const LoginPage = () => {
     const [resText, setResText] = useState("");
@@ -29,38 +27,32 @@ const LoginPage = () => {
                 console.log(error);
             });
     };
+
     return (
 
         <div>
-            <view className="AllPage" style="background-image: url({background});">
+            <view className="AllPage">
                 <view className="content">
                     <view className="userProfilePhotoArea">
-                        <view className="userProfilePhotoBox">
-                            <img className="userProfilePhoto" src="{profilePhoto}"/>
-                        </view>
                     </view>
                     <form onSubmit={handleSubmit}>
                         <view className="inputArea">
                             <view className="account">
                                 <input type="text" name="id" bindinput="EmailInput" placeholder="请输入账号"
-                                       placeholder-style="font-size: 1.2rem;"/>
+                                    placeholder-style="font-size: 1.2rem;" />
                             </view>
                             <view className="password">
                                 <input type="password" name="password" bindinput="passwordInput"
-                                       placeholder=" 请输入密码" placeholder-style="font-size: 1.2rem;"/>
+                                    placeholder=" 请输入密码" placeholder-style="font-size: 1.2rem;" />
                             </view>
-                        </view>
-                        <view className="navigator">
-                            <navigator url="#" open-type="navigate">注册账号</navigator>
-                            <navigator url="#">忘记密码</navigator>
                         </view>
                         <view className="buttonArea">
                             <input type="submit" value="登录" bindtap="Login" className="loginButton"></input>
                         </view>
+                        <text>{resText}</text>
                     </form>
                 </view>
             </view>
-            <h3>{resText}</h3>
         </div>
     )
 }
