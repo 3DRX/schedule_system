37463f8212840da0406144aaed59867b1ac6c02d
3DRX <kjy20030228@icloud.com>
2023-02-27 15:34:40 +0800

    添加Admin中addCourseForStudent方法

--- app/src/main/java/schedule_system/Admin.java
+++ app/src/main/java/schedule_system/Admin.java
@@ -1,7 +1,28 @@
 package schedule_system;
 
+import schedule_system.utils.Course;
+
 /**
  * Admin
  */
 public class Admin {
+    /**
+     * 为学生添加课程
+     * 
+     * @param course
+     * @param student
+     * @return ifSuccess
+     */
+    public boolean addCourseForStudent(Course course, Student student) {
+        student.addCourse(course);
+        return true;
+    }
+
+    public Course getCourseOfStudent(Student student, String courseName){
+        return student.getCourseByName(courseName);
+    }
+
+    public Student[] getAllStudents(){
+        return System.getStudents();
+    }
 }
