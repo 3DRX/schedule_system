7cfc156e40ca8e77c8b7864a6da86acd71c17751
3DRX <kjy20030228@icloud.com>
2023-03-04 19:58:39 +0800

    完成管理员界面的课表

--- app/src/main/frontend/src/pages/StudentCourse.jsx
+++ app/src/main/frontend/src/pages/StudentCourse.jsx
@@ -1,112 +1,16 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
-import { useTable } from "react-table";
+import ClassTable from "../components/ClassTable";
 import NavBar from "../components/NavBar";
-import TableCell from "../components/TableCell";
 
 const StudentCourse = () => {
     const query = new URLSearchParams(useLocation().search);
     const userName = query.get("userName");
 
-    const data = React.useMemo(
-        () => {
-            let res = [];
-            for (let i = 8; i <= 19; i++) {
-                let cell = { col0: i + '-' + (i + 1) };
-                for (let j = 1; j <= 5; j++) {
-                    let key = "col" + j;
-                    cell[key] = <TableCell startTime={i} week={3} day={j} userName={userName} />;
-                }
-                res.push(cell);
-            }
-            return res;
-        }, []
-    )
-
-    const columns = React.useMemo(
-        () => [
-            {
-                Header: '',
-                accessor: 'col0', // accessor is the "key" in the data
-            },
-            {
-                Header: '一',
-                accessor: 'col1', // accessor is the "key" in the data
-            },
-            {
-                Header: '二',
-                accessor: 'col2',
-            },
-            {
-                Header: '三',
-                accessor: 'col3',
-            },
-            {
-                Header: '四',
-                accessor: 'col4',
-            },
-            {
-                Header: '五',
-                accessor: 'col5',
-            },
-        ], []
-    )
-
-    const {
-        getTableProps,
-        getTableBodyProps,
-        headerGroups,
-        rows,
-        prepareRow,
-    } = useTable({ columns, data });
-
     return (
         <>
             <NavBar isAdmin="false" userName={userName} />
-            <table {...getTableProps()} style={{ border: 'solid 1px blue' }}>
-                <thead>
-                    {headerGroups.map(headerGroup => (
-                        <tr {...headerGroup.getHeaderGroupProps()}>
-                            {headerGroup.headers.map(column => (
-                                <th
-                                    {...column.getHeaderProps()}
-                                    style={{
-                                        borderBottom: 'solid 3px red',
-                                        background: 'aliceblue',
-                                        color: 'black',
-                                        fontWeight: 'bold',
-                                    }}
-                                >
-                                    {column.render('Header')}
-                                </th>
-                            ))}
-                        </tr>
-                    ))}
-                </thead>
-                <tbody {...getTableBodyProps()}>
-                    {rows.map(row => {
-                        prepareRow(row)
-                        return (
-                            <tr {...row.getRowProps()}>
-                                {row.cells.map(cell => {
-                                    return (
-                                        <td
-                                            {...cell.getCellProps()}
-                                            style={{
-                                                padding: '10px',
-                                                border: 'solid 1px gray',
-                                                background: 'white',
-                                            }}
-                                        >
-                                            {cell.render('Cell')}
-                                        </td>
-                                    )
-                                })}
-                            </tr>
-                        )
-                    })}
-                </tbody>
-            </table>
+            <ClassTable isAdmin={false} />
         </>
     )
 }
