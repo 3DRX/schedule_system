f27d00d85183adb7730e8942dcf6c7015e1414d1
3DRX <kjy20030228@icloud.com>
2023-03-09 15:36:48 +0800

    添加删除课程的按钮

--- app/src/main/frontend/src/components/TableCell.jsx
+++ app/src/main/frontend/src/components/TableCell.jsx
@@ -1,15 +1,21 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import axios from "axios";
 import "./TableCell.css";
 import Button from 'react-bootstrap/Button';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import Popover from 'react-bootstrap/Popover';
+import Modal from 'react-bootstrap/Modal';
+import Overlay from 'react-bootstrap/Overlay';
 
 export default function TableCell({ startTime, week, day, userName, isAdmin, setShowModal, setAddClassInfo, refresh }) {
     const [name, setName] = useState("");
     const [location, setLocation] = useState("");
     const [students, setStudents] = useState([]);
     const [hover, setHover] = useState(false);
+    const [showDeleteConfirm, setShowDeleteConfirm] = useState(false);
+    const [showOverlay, setShowOverlay] = useState(false);
+    const [refreshOnDel, setRefreshOnDel] = useState(false);
+    const target = useRef(null);
 
     useEffect(() => {
         axios.get("http://" + window.location.hostname + ":8080/getCourseStatusByTime", {
@@ -26,11 +32,23 @@ export default function TableCell({ startTime, week, day, userName, isAdmin, set
                 setLocation(response.data.location)
                 setStudents(response.data.students)
             })
-    }, [startTime, week, day, userName, refresh]);
+    }, [startTime, week, day, userName, refresh, refreshOnDel]);
+
+    const setDeleteCourseRequest = () => {
+        axios.delete(`http://localhost:8080/deleteCourse?courseName=${name}`)
+            .then((response) => {
+                if (response) {
+                    setRefreshOnDel(!refreshOnDel);
+                }
+                else {
+                    console.warn(`删除课程${name}失败`);
+                }
+            });
+    }
 
     let studentsHover;
     if (isAdmin) {
-        if (students.length === 0) {
+        if (name === "") {
             studentsHover = (
                 <div onClick={() => {
                     // console.log(`在第${week}周，周${day}，${startTime}-${startTime + 1}添加课程`);
@@ -53,21 +71,28 @@ export default function TableCell({ startTime, week, day, userName, isAdmin, set
         else {
             let studentList = students.map((item, index) => {
                 return (
-                    <a key={index}>{item}{index === students.length - 1 ? "" : ","}</a>
+                    <li key={index}>{item}</li>
                 )
             })
-            const popover = (
-                <Popover id="popover-basic">
-                    <Popover.Header as="h3">Students</Popover.Header>
-                    <Popover.Body>
-                        {studentList}
-                    </Popover.Body>
-                </Popover>
-            );
             studentsHover = (
-                <OverlayTrigger trigger="click" placement="top" overlay={popover}>
-                    <Button className="btn" variant="info" >detail</Button>
-                </OverlayTrigger>
+                <>
+                    <Button className="btn" variant="info" ref={target} onClick={() => setShowOverlay(!showOverlay)}>detail</Button>
+                    <Overlay target={target.current} show={showOverlay} placement="right">
+                        <Popover id="popover-basic" show={showOverlay}>
+                            <Popover.Header as="h3">{name}</Popover.Header>
+                            <Popover.Body>
+                                <h5>Students</h5>
+                                <ul>
+                                    {studentList}
+                                </ul>
+                                <Button className="btn" variant="outline-danger" size="sm" onClick={() => {
+                                    setShowOverlay(false);
+                                    setShowDeleteConfirm(true);
+                                }}>delete</Button>
+                            </Popover.Body>
+                        </Popover>
+                    </Overlay>
+                </>
             )
         }
     }
@@ -80,6 +105,29 @@ export default function TableCell({ startTime, week, day, userName, isAdmin, set
             <p>{name}</p>
             <p>{location}</p>
             {studentsHover}
+            <Modal
+                show={showDeleteConfirm}
+                onHide={() => setShowDeleteConfirm(false)}
+                backdrop="static"
+                keyboard={false}
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title>Modal title</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    I will not close if you click outside me. Don't even try to press
+                    escape key.
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={() => setShowDeleteConfirm(false)}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={() => {
+                        setDeleteCourseRequest();
+                        setShowDeleteConfirm(false);
+                    }}>确认删除课程</Button>
+                </Modal.Footer>
+            </Modal>
         </div>
     );
 }
