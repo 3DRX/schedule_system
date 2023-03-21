5f9d41bf5fdfbfaba4c93f96f8b7062de2a82054
3DRX <kjy20030228@icloud.com>
2023-03-21 09:08:16 +0800

    添加示例

--- app/src/test/java/schedule_system/ConflictCheckTest.java
+++ app/src/test/java/schedule_system/ConflictCheckTest.java
@@ -2,6 +2,10 @@ package schedule_system;
 
 import org.junit.jupiter.api.Test;
 
+import schedule_system.utils.ClassTime;
+import schedule_system.utils.Course;
+import schedule_system.utils.Location;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Disabled;
@@ -10,5 +14,14 @@ public class ConflictCheckTest {
     @Test
     void test1() {
         // TODO
+        // 创建两个新的Course
+        Course course1 = new Course(1, 10, 11, new ClassTime(1, 8, 3), new ClassTime(1, 8, 3), "course1",
+                new Location());
+        Course course2 = new Course(1, 10, 11, new ClassTime(1, 8, 3), new ClassTime(1, 8, 3), "course2",
+                new Location());
+        // 显然，这里两个课程是冲突的，所以有下面这一行代码。
+        assertTrue(course1.conflictsWith(course2));
+        // 我们要测试的函数就是这个conflictsWith，
+        // 此处如果这个函数的判断有问题，测试用例就不会通过。
     }
 }
