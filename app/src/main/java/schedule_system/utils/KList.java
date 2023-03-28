0debc88815eaf59776970ed9eed9f7cc2abd7fa6
3DRX <kjy20030228@icloud.com>
2023-03-28 23:14:56 +0800

    add KList.java

---
+++ app/src/main/java/schedule_system/utils/KList.java
@@ -0,0 +1,44 @@
+package schedule_system.utils;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+/**
+ * KList
+ */
+public class KList<T> {
+    private T[] list;
+
+    public KList(Class<T> t) {
+        this.list = (T[]) Array.newInstance(t, 0);
+    }
+
+    public void add(T t) {
+        list = Arrays.copyOf(list, list.length + 1);
+        list[list.length - 1] = t;
+    }
+
+    public T get(int i) {
+        return list[i];
+    }
+
+    public T popLeft() {
+        T res = list[0];
+        list = Arrays.copyOfRange(list, 1, list.length);
+        return res;
+    }
+
+    public T peekLeft() {
+        return list[0];
+    }
+
+    public T popRight() {
+        T res = list[list.length - 1];
+        list = Arrays.copyOfRange(list, 0, list.length - 1);
+        return res;
+    }
+
+    public T peekRight() {
+        return list[list.length - 1];
+    }
+}
