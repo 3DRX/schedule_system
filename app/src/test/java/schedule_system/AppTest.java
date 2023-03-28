413ac2b238b127fbdf02226df15d937580c30143
3DRX <kjy20030228@icloud.com>
2023-03-28 09:08:12 +0800

    .

--- app/src/test/java/schedule_system/AppTest.java
+++ app/src/test/java/schedule_system/AppTest.java
@@ -2,21 +2,7 @@ package schedule_system;
 
 import org.junit.jupiter.api.Test;
 
-import schedule_system.fakeDB.CourseData;
-import schedule_system.utils.Course;
-import schedule_system.utils.KMap;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
-
-    @Test
-    void test1() {
-        CourseData courseData = new CourseData();
-        KMap<String, Course> map = new KMap<>();
-        for (Course course : courseData.allCourses()) {
-            map.put(course.getName(), course);
-        }
-        System.out.println(map.get("java").getLocation().getName());
-    }
 }
