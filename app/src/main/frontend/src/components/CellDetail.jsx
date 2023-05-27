d3a66a4fcc9d2ccc67e61339d3811e72af6a658a
Leonida <2664887227@qq.com>
2023-05-27 23:22:44 +0800

    课外活动删除按钮

--- app/src/main/frontend/src/components/CellDetail.jsx
+++ app/src/main/frontend/src/components/CellDetail.jsx
@@ -9,7 +9,8 @@ const CellDetail = ({ course, refresh, setRefresh, isAdmin }) => {
     const [showOverlay, setShowOverlay] = useState(false);
     const [showDeleteConfirm, setShowDeleteConfirm] = useState(false);
     const target = useRef(null);
-
+    const [showActivityDetail, setShowActivityDetail] = useState(false);
+    const [showDeleteActivityModal, setShowDeleteActivityModal] = useState(false);
     // 发送删除课程的请求
     const setDeleteCourseRequest = () => {
         axios.delete("http://" + window.location.hostname + `:8888/deleteCourse?courseName=${course.name}`)
@@ -64,6 +65,49 @@ const CellDetail = ({ course, refresh, setRefresh, isAdmin }) => {
                             <Button variant="primary" onClick={() => {
                                 setDeleteCourseRequest();
                                 setShowDeleteConfirm(false);
+                            }}>确认删除</Button>
+                        </Modal.Footer>
+                    </Modal>
+                </>
+            )
+        }
+        else if(course.isActivity)
+        {
+            return (
+                <>
+                    <Button className="btn" variant="info" ref={target} onClick={() => {}}>detail</Button>
+                    <Overlay target={target.current} show={showActivityDetail} placement="right">
+                        <Popover id="popover-basic" show={showActivityDetail}>
+                            <Popover.Header as="h3">{course.name}</Popover.Header>
+                            <Popover.Body>
+                                <h5>Students</h5>
+                                <ul>
+                                    {course.students.map((student) => {
+                                        return <li>{student}</li>
+                                    })}
+                                </ul>
+                                <Button className="btn" variant="outline-danger" size="sm" onClick={() => {
+                                }}>delete</Button>
+                            </Popover.Body>
+                        </Popover>
+                    </Overlay>
+                    <Modal
+                        show={showDeleteActivityModal}
+                        onHide={() => {}}
+                        backdrop="static"
+                        keyboard={false}
+                    >
+                        <Modal.Header closeButton>
+                            <Modal.Title>删除课程：{course.name}</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            此课程一经删除，无法恢复。
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button variant="secondary" onClick={() => {}}>
+                                取消
+                            </Button>
+                            <Button variant="primary" onClick={() => {
                             }}>确认删除</Button>
                         </Modal.Footer>
                     </Modal>
