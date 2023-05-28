d2e5c1d2c5333414b76d8294733e9acfcc91a99f
Leonida <2664887227@qq.com>
2023-05-29 00:56:18 +0800

    日志界面css

--- app/src/main/frontend/src/pages/AdminOthers.jsx
+++ app/src/main/frontend/src/pages/AdminOthers.jsx
@@ -6,6 +6,7 @@ import { Table } from 'antd';
 import axios from "axios";
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import "./AdminOthers.css";
 
 const columns = [
     {
@@ -77,79 +78,101 @@ const AdminOthers = () => {
     return (
         <>
             <NavBar isAdmin="true" userName={userName} />
-            <h1>日志查看</h1>
-            <div>
-                <div>日期</div>
-                <NumberPicker defaultValue={year} step={1} max={Infinity} min={2000} onChange={(value) => {
-                    if (value !== null && value >= 2000) {
-                        setYear(value);
-                    }
-                }}
-                    style={{
-                        width: "12ex",
-                    }}
-                />
-                <NumberPicker defaultValue={month} step={1} max={12} min={1} onChange={(value) => {
-                    if (value !== null && value >= 1 && value <= 12) {
-                        setMonth(value);
-                    }
-                }}
-                    style={{
-                        width: "10ex",
-                    }}
-                />
-                <NumberPicker defaultValue={day} step={1} max={31} min={1} onChange={(value) => {
-                    if (value !== null && value >= 1 && value <= 31) {
-                        setDay(value);
-                    }
-                }}
-                    style={{
-                        width: "10ex",
-                    }}
-                />
-                <div>时间</div>
-                <NumberPicker defaultValue={startTime} step={1} max={23} min={0} onChange={(value) => {
-                    if (value !== null && value >= 0 && value <= 23) {
-                        setStartTime(value);
-                    }
-                }}
-                    style={{
-                        width: "10ex",
-                    }}
-                />
-                <NumberPicker defaultValue={endTime} step={1} max={23} min={0} onChange={(value) => {
-                    if (value !== null && value >= 0 && value <= 23) {
-                        setEndTime(value);
-                    }
-                }}
-                    style={{
-                        width: "10ex",
-                    }}
-                />
-                <div>类型</div>
-                <Form.Select aria-label="Default select example"
-                    onChange={({ target: { value } }) => {
-                        if (value === "1") {
-                            setLevel("INFO");
-                        }
-                        else if (value === "2") {
-                            setLevel("WARN");
-                        }
-                        else if (value === "3") {
-                            setLevel("ERROR");
-                        }
-                    }}
-                    style={{ width: "100px" }}
-                >
-                    <option value="1">INFO</option>
-                    <option value="2">WARN</option>
-                    <option value="3">ERROR</option>
-                </Form.Select>
-                <Button variant="primary" onClick={() => setRefresh(!refresh)}>
-                    刷新
-                </Button>
+            <div id="logPageContent">
+                <h1>日志查看</h1>
+                <div id="logPageSettings">
+                    <div id="logDay">
+                        <div className="logSettingText">日期</div>
+                        <NumberPicker defaultValue={year} step={1} max={Infinity} min={2000} onChange={(value) => {
+                            if (value !== null && value >= 2000) {
+                                setYear(value);
+                            }
+                        }}
+                                      style={{
+                                          width: "12ex",
+                                      }}
+                        />
+                        <div className="logSettingText">年</div>
+                        <NumberPicker defaultValue={month} step={1} max={12} min={1} onChange={(value) => {
+                            if (value !== null && value >= 1 && value <= 12) {
+                                setMonth(value);
+                            }
+                        }}
+                                      style={{
+                                          width: "10ex",
+                                      }}
+                        />
+                        <div className="logSettingText">月</div>
+                        <NumberPicker defaultValue={day} step={1} max={31} min={1} onChange={(value) => {
+                            if (value !== null && value >= 1 && value <= 31) {
+                                setDay(value);
+                            }
+                        }}
+                                      style={{
+                                          width: "10ex",
+                                      }}
+                        />
+                        <div className="logSettingText">日</div>
+
+                    </div>
+
+                    <div id="logTime">
+                        <div className="logSettingText">时间</div>
+                        <NumberPicker defaultValue={startTime} step={1} max={23} min={0} onChange={(value) => {
+                            if (value !== null && value >= 0 && value <= 23) {
+                                setStartTime(value);
+                            }
+                        }}
+                                      style={{
+                                          width: "10ex",
+                                      }}
+                        />
+                        <div className="logSettingText">点~</div>
+                        <NumberPicker defaultValue={endTime} step={1} max={23} min={0} onChange={(value) => {
+                            if (value !== null && value >= 0 && value <= 23) {
+                                setEndTime(value);
+                            }
+                        }}
+                                      style={{
+                                          width: "10ex",
+                                      }}
+                        />
+                        <div className="logSettingText">点</div>
+                    </div>
+                    <div id="logType">
+                        <div className="logSettingText">类型</div>
+                        <Form.Select aria-label="Default select example"
+                                     onChange={({ target: { value } }) => {
+                                         if (value === "1") {
+                                             setLevel("INFO");
+                                         }
+                                         else if (value === "2") {
+                                             setLevel("WARN");
+                                         }
+                                         else if (value === "3") {
+                                             setLevel("ERROR");
+                                         }
+                                     }}
+                                     style={{ width: "100px" }}
+                        >
+                            <option value="1">INFO</option>
+                            <option value="2">WARN</option>
+                            <option value="3">ERROR</option>
+                        </Form.Select>
+                    </div>
+                    <div>
+                        <Button variant="primary" onClick={() => setRefresh(!refresh)}>
+                            刷新
+                        </Button>
+                    </div>
+
+                </div>
+                <div id="logTableBox">
+                    <Table id="logTable" columns={columns} dataSource={data} />
+                </div>
+
             </div>
-            <Table columns={columns} dataSource={data} />
+
         </>
     )
 }
