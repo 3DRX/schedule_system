1191b17de62c0819230c5872adcff57e48057480
3DRX <kjy20030228@icloud.com>
2023-05-29 22:08:10 +0800

    add animated background with vanta

--- app/src/main/frontend/src/pages/LoginPage.jsx
+++ app/src/main/frontend/src/pages/LoginPage.jsx
@@ -1,10 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from 'react'
+import BIRDS from 'vanta/dist/vanta.birds.min';
 import axios from 'axios';
 import './LoginPage.css';
 import { Button } from 'react-bootstrap';
 
 const LoginPage = () => {
     const [resText, setResText] = useState("");
+    const [vantaEffect, setVantaEffect] = useState(null)
+    const myRef = useRef(null)
+
+    useEffect(() => {
+        if (!vantaEffect) {
+            setVantaEffect(BIRDS({
+                el: myRef.current,
+                mouseControls: true,
+                touchControls: true,
+                gyroControls: false,
+                minHeight: 200.00,
+                minWidth: 200.00,
+                scale: 1.00,
+                scaleMobile: 1.00,
+                backgroundColor: 0xffffff,
+                color1: 0x796400,
+                color2: 0xcf8e03,
+                birdSize: 2.40,
+                wingSpan: 29.00,
+                quantity: 3.00,
+                backgroundAlpha: 0.00
+            }))
+        }
+        return () => {
+            if (vantaEffect) vantaEffect.destroy()
+        }
+    }, [vantaEffect])
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -35,41 +63,39 @@ const LoginPage = () => {
 
     return (
         <div>
-            <div className="AllPage">
-                <div className="LoginPageContent">
-                    <div className="userProfilePhotoArea">
-                    </div>
-                    <form onSubmit={handleSubmit}>
-                        <div id="SMS"><h1>学生日程管理系统</h1></div>
-                        <div className="inputArea">
-                            <div className="account">
-                                <input type="text" name="id" bindinput="EmailInput" placeholder="请输入账号"
-                                    placeholder-style="font-size: 1.2rem;" />
-                            </div>
-                            <div className="password">
-                                <input type="password" name="password" bindinput="passwordInput"
-                                    placeholder=" 请输入密码" placeholder-style="font-size: 1.2rem;" />
-                            </div>
-                        </div>
-                        <div className="buttonArea">
-                            <Button onClick={() => {
-                                window.open("http://" + window.location.host + "/register", "_self");
-                            }} id="startButton" className="loginButton">
-                                <span className="shadow"></span>
-                                <span className="edge" id="registerEdge"></span>
-                                <span className="front text" id="registerFront">注册</span>
-                            </Button>
-                            <Button type="submit" id="startButton" className="loginButton">
-                                <span className="shadow"></span>
-                                <span className="edge"></span>
-                                <span className="front text">登录</span>
-                            </Button>
+        <div className="AllPage" ref={myRef}>
+        <div className="LoginPageContent">
+        <form onSubmit={handleSubmit}>
+        <div id="SMS"><h1>学生日程管理系统</h1></div>
+        <div className="inputArea">
+        <div className="account">
+        <input type="text" name="id" bindinput="EmailInput" placeholder="请输入账号"
+        placeholder-style="font-size: 1.2rem;" />
+        </div>
+        <div className="password">
+        <input type="password" name="password" bindinput="passwordInput"
+        placeholder=" 请输入密码" placeholder-style="font-size: 1.2rem;" />
+        </div>
+        </div>
+        <div className="buttonArea">
+        <Button onClick={() => {
+            window.open("http://" + window.location.host + "/register", "_self");
+        }} id="startButton" className="loginButton">
+        <span className="shadow"></span>
+        <span className="edge" id="registerEdge"></span>
+        <span className="front text" id="registerFront">注册</span>
+        </Button>
+        <Button type="submit" id="startButton" className="loginButton">
+        <span className="shadow"></span>
+        <span className="edge"></span>
+        <span className="front text">登录</span>
+        </Button>
 
-                        </div>
-                        <div id="loginFail">{resText}</div>
-                    </form>
-                </div>
-            </div>
+        </div>
+        <div id="loginFail">{resText}</div>
+        </form>
+        </div>
+        </div>
         </div>
     );
 };
